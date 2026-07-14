8ef1b95246d77b85d7b7aedde3b665f9e50c42d1
--global <me@metehanselvi.com>
2026-07-14 22:21:40 +0000

    tests(EmailLimitingRepoImpl): `refund_ip_quota`/`clear_email_limit`

--- tests/src/email_limiting_repo.rs
+++ tests/src/email_limiting_repo.rs
@@ -20,6 +20,44 @@ pub async fn email_limiting(repo: &dyn EmailLimitingRepo) -> RepoResult<()> {
         EmailLimitingResult::EmailLimited(..) | EmailLimitingResult::IpLimited(..)
     );
 
+    // Now the IP can send another email. But the address is still blocked
+    repo.refund_ip_quota(&ip, email).await?;
+    let another_email = random_email();
+
+    assert_matches!(
+        repo.check_and_consume_quota(&ip, email).await?,
+        EmailLimitingResult::EmailLimited(..)
+    );
+
+    assert_matches!(
+        repo.check_and_consume_quota(&ip, another_email).await?,
+        EmailLimitingResult::Allowed
+    );
+
+    // Clear the limits on `another_email`
+    repo.clear_email_limit(another_email).await?;
+    let another_ip = random_ipv6();
+
+    assert_matches!(
+        repo.check_and_consume_quota(&ip, another_email).await?,
+        EmailLimitingResult::IpLimited(..)
+    );
+
+    assert_matches!(
+        repo.check_and_consume_quota(&another_ip, another_email)
+            .await?,
+        EmailLimitingResult::Allowed
+    );
+
+    // This does not unblock the IP, as it is blocked due to `email`
+    repo.refund_ip_quota(&another_ip, email).await?;
+    let yet_another_email = random_email();
+
+    assert_matches!(
+        repo.check_and_consume_quota(&ip, yet_another_email).await?,
+        EmailLimitingResult::IpLimited(..)
+    );
+
     Ok(())
 }
 
