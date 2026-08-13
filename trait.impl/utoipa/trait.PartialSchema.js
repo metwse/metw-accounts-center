a8cf4d86b24d595905e5aee29ae196a0951ab4b4
metwse <me@metehanselvi.com>
2026-08-13 23:57:27 +0000

    Deploying to autogen/gh-pages from @ metwse/metw-accounts-center@41c96e7e9a30ca53b9faacacdeca66e3a5fe47e0 🚀

--- trait.impl/utoipa/trait.PartialSchema.js
+++ trait.impl/utoipa/trait.PartialSchema.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["service",[["impl PartialSchema for <a class=\"struct\" href=\"service/util/id/struct.AccountId.html\" title=\"struct service::util::id::AccountId\">AccountId</a>",0]]]]);
+    const implementors = Object.fromEntries([["service",[["impl PartialSchema for <a class=\"struct\" href=\"service/util/id/struct.AccountId.html\" title=\"struct service::util::id::AccountId\">AccountId</a>",0],["impl PartialSchema for <a class=\"enum\" href=\"service/dto/request/enum.AccountIdentifier.html\" title=\"enum service::dto::request::AccountIdentifier\">AccountIdentifier</a>",0],["impl PartialSchema for <a class=\"enum\" href=\"service/dto/request/enum.PublicAccountIdentifier.html\" title=\"enum service::dto::request::PublicAccountIdentifier\">PublicAccountIdentifier</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[169]}
+//{"start":59,"fragment_lengths":[551]}
