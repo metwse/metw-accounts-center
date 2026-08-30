5de25da8ae525a94448b867200f34c41f0000b9a
metwse <me@metehanselvi.com>
2026-08-30 23:00:35 +0000

    Deploying to autogen/gh-pages from @ metwse/metw-accounts-center@ff1aa05ee27c33d5db82a51fdff3bc86dc3f2b10 🚀

--- service/dto/response/sidebar-items.js
+++ service/dto/response/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Account","AccountKdf","ApplicationSummary","CreatedApplication","RotatedClientSecret","Token"]};
+window.SIDEBAR_ITEMS = {"struct":["Account","AccountKdf","ApplicationConsent","ApplicationConsentStatus","ApplicationSummary","CreatedApplication","RotatedClientSecret","Token"]};
